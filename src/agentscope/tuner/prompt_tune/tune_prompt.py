96895026ec92e6d9df25935e37a675fb534c6208
cc <cc445845@alibaba-inc.com>
2026-01-08 16:10:16 +0800

    refactor(tuner): add config and logs for prompt tuning

--- src/agentscope/tuner/prompt_tune/tune_prompt.py
+++ src/agentscope/tuner/prompt_tune/tune_prompt.py
@@ -1,32 +1,23 @@
 
-from agentscope.message import Msg
-from agentscope.formatter import OpenAIChatFormatter
 from agentscope.agent import ReActAgent
 from agentscope.tuner import (
-    tune,
     Dataset,
-    WorkflowOutput,
-    JudgeOutput,
     TunerChatModel,
-    Algorithm,
 )
-from typing import Dict
 import asyncio
-import os
 from typing import Any, Callable
 
 from datasets import load_dataset
 import dspy
 from agentscope import logger
 from agentscope.agent._react_agent import ReActAgent
-from agentscope.model._dashscope_model import DashScopeChatModel
 from agentscope.model._model_base import ChatModelBase
 from agentscope.tuner._config import check_judge_function
 from agentscope.tuner._model import TunerChatModel
 from agentscope.tuner._workflow import WorkflowType
 from agentscope.tuner._judge import JudgeType
 from agentscope.tuner._dataset import Dataset
-from agentscope.tuner._algorithm import Algorithm
+from agentscope.tuner.prompt_tune.config import PromptTuneConfig
 from agentscope.tuner.prompt_tune.wrapper import WorkflowWrapperModule
 
 
@@ -50,24 +41,28 @@ def tune_prompt(
     eval_dataset: Dataset | None = None,
     model: ChatModelBase,
     auxiliary_models: dict[str, ChatModelBase] | None = None,
-)->ReActAgent:
+    config: PromptTuneConfig | None = None,
+) -> ReActAgent:
+    config = config or PromptTuneConfig()
     auxiliary_models = auxiliary_models or {}
     if isinstance(model, TunerChatModel):
         logger.warning(
-            "Model is a TunerChatModel, which will not be optimized during prompt tuning.")
+            "Model is a TunerChatModel, which will not be optimized during prompt tuning.",
+        )
     check_judge_function(judge_func)
-
+    
+    logger.info("loading training dataset...")
     trainset = load_dataset(
         path=train_dataset.path,
         name=train_dataset.name,
         split=train_dataset.split,
         streaming=True,
     )
-    
+    logger.info("training dataset loaded")
+
     dspy_trainset = [
-        dspy.Example(inp=x).with_inputs('inp')
-        for x in trainset
-    ][:16] # FIXME
+        dspy.Example(inp=x).with_inputs("inp") for x in trainset
+    ]
 
     module = WorkflowWrapperModule(workflow_func, init_agent)
     # model and auxiliary_models are not necessary for prompt tuning.
@@ -75,50 +70,57 @@ def tune_prompt(
     module._set_chatmodel(model, auxiliary_models)
 
     # teacher lm
-    if 'DASHSCOPE_API_KEY' in os.environ:
-        lm = dspy.LM('dashscope/qwen-plus')
-    else:
-        lm = dspy.LM('openai/gpt-4o')
+    lm = dspy.LM(config.lm_model_name)
 
     optimizer = dspy.MIPROv2(
-        metric=(lambda data, output, trace=None: wrap_judge_fn(
-            judge_func)(data.inp, output, auxiliary_models)),
-        auto='light',
+        metric=(
+            lambda data, output, trace=None: wrap_judge_fn(judge_func)(
+                data.inp,
+                output,
+                auxiliary_models,
+            )
+        ),
+        auto=config.optimization_level,
         teacher_settings={
-            'lm': lm,
+            "lm": lm,
         },
-        prompt_model=lm, # TODO: what is this
-        task_model=lm, # TODO: what is this
+        prompt_model=lm,
+        task_model=lm,
     )
 
     # optimize
+    logger.info("optimizing workflow...")
     result = optimizer.compile(module, trainset=dspy_trainset)
+    logger.info("workflow optimized")
 
     # evaluate if eval_dataset is provided
     if eval_dataset is not None:
+        logger.info("loading evaluation dataset...")
         evalset = load_dataset(
             path=eval_dataset.path,
             name=eval_dataset.name,
             split=eval_dataset.split,
             streaming=True,
         )
+        logger.info("evaluation dataset loaded")
 
         dspy_evalset = [
-            dspy.Example(inp=x).with_inputs('inp')
-            for x in evalset
-        ][:16] # FIXME
+            dspy.Example(inp=x).with_inputs("inp") for x in evalset
+        ]
 
         evaluate = dspy.Evaluate(
             devset=dspy_evalset,
             metric=lambda data, output, trace=None: wrap_judge_fn(
-                judge_func)(data.inp, output, auxiliary_models),
-            display_progress=True,
-            display_table=5,
-            num_threads=16,
+                judge_func,
+            )(data.inp, output, auxiliary_models),
+            display_progress=config.eval_display_progress,
+            display_table=config.eval_display_table,
+            num_threads=config.eval_num_threads,
         )
 
+        logger.info("evaluating results...")
         eval_res = evaluate(result)
         score = eval_res.score
-        print(f"Evaluation score: {score}")
+        logger.info(f"evaluation score: {score}")
 
     return result._agent
