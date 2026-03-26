208afbc545298b471b165e853ec2727a8e7c62d8
sztoplover-bit <253473756+sztoplover-bit@users.noreply.github.com>
2026-03-26 16:20:22 +0800

    style(tests): align tool tests with pre-commit black

--- tests/tool_test.py
+++ tests/tool_test.py
@@ -33,7 +33,9 @@ class ToolTest(IsolatedAsyncioTestCase):
         # empty output
         res = await execute_python_code(code="a = 1 + 1")
         self.assertEqual(
-            "<returncode>0</returncode>" "<stdout></stdout>" "<stderr></stderr>",
+            "<returncode>0</returncode>"
+            "<stdout></stdout>"
+            "<stderr></stderr>",
             res.content[0]["text"],
         )
 
@@ -152,7 +154,8 @@ print("456")"""
 
         # large output should not deadlock on pipe buffers
         large_output_cmd = (
-            f'{sys.executable} -c "import sys; ' f"sys.stdout.write('x' * 131072)\""
+            f'{sys.executable} -c "import sys; '
+            f"sys.stdout.write('x' * 131072)\""
         )
         res = await execute_shell_command(
             command=large_output_cmd,
@@ -165,7 +168,9 @@ print("456")"""
             ),
         )
         self.assertIn("</stdout><stderr></stderr>", actual)
-        stdout_payload = actual.split("<stdout>", 1)[1].split("</stdout>", 1)[0]
+        stdout_payload = actual.split("<stdout>", 1)[1].split("</stdout>", 1)[
+            0
+        ]
         self.assertEqual(131072, len(stdout_payload))
 
     async def test_view_text_file(self) -> None:
