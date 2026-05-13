a629336a775b7c28c53e498f845bb9c244eec6fa
Wei Zhang <136773451+weizhang25@users.noreply.github.com>
2026-05-13 22:10:42 +0000

    Add init file

---
+++ src/agentscope/codeact/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""This module provides CodeAct problem solving ability to agent."""
+
+from .code_act_run_tool_server import (
+    CodeActRunToolServer,
+    RunToolRequest,
+)
+
+
+__all__ = [
+    "CodeActRunToolServer",
+    "RunToolRequest",
+]
